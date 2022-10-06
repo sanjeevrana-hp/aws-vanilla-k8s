ee863eb22f49409d675e6a9c4265a1efc2b54ce7
Sanjeev Rana <sanjeevnoida@gmail.com>
2022-10-06 23:31:12 +0530

    1.3

--- instance.tf
+++ instance.tf
@@ -62,25 +62,26 @@ resource "local_file" "KeyPair_File" {
 }
 
 ####### CREATING THE EC2 Compute ###########
+
 resource "aws_instance" "k8s" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = var.instance_type
   count           = 2
   key_name        = "${var.name}-${random_pet.username.id}-KeyPair"
-  security_groups = ["{aws_security_group.allow-all-security-group.id}"]
+  security_groups = ["${aws_security_group.allow-all-security-group.name}"]
   root_block_device {
     volume_size           = "50"
     delete_on_termination = "true"
   }
   user_data = <<EOF
-    #!/bin/bash
-    cd /var/tmp/
-    apt-get update -y
-    apt-get install wget
-    wget https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_master.sh
-    wget https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_worker.sh
-    chmod 700 install_master.sh
-    chmod 700 install_worker.sh
+#!/bin/bash
+cd /var/tmp/
+apt-get update -y
+apt-get install wget
+wget https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_master.sh
+wget https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_worker.sh
+chmod 700 install_master.sh
+chmod 700 install_worker.sh
 EOF
   tags = {
     Name           = var.host_names[count.index]
