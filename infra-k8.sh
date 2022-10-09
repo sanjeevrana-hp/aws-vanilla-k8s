19a1d2619ff0073bd5f230bb9a7119ed468c4fef
Sanjeev Rana <sanjeevnoida@gmail.com>
2022-10-09 16:29:21 +0530

    version 1.5

--- infra-k8.sh
+++ infra-k8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case $1 in
-        "--create")
+        "--create-cluster")
                 echo "Creating the EC2 Instances, and installing the K8s"
 		cd /terraform
                 terraform init
@@ -12,7 +12,27 @@ case $1 in
 		cd /terraform
 		terraform output
                 ;;
-        "--delete")
+
+        "--create-msrv3")
+                echo "Creating the EC2 Instances, and installing the K8s"
+                cd /terraform
+                terraform init
+                terraform apply --auto-approve
+                sleep 45
+                cd /terraform/ansible
+                ansible-playbook master-kubernet.yaml
+                ansible-playbook worker-kubernet.yaml
+		sleep 15
+		cd /terraform/ansible/storage-classes
+		ansible-playbook storage-classes.yaml
+		sleep 15
+		cd /terraform/ansible/MSRv3
+		ansible-playbook  msrv3-install.yaml
+		cd /terraform
+		terraform output
+                ;;
+
+        "--delete-cluster")
                 echo "Deleting K8s, and EC2"
 		cd /terraform
                 terraform destroy --auto-approve
@@ -22,3 +42,14 @@ case $1 in
                 echo
                 ;;
 esac
+
+Help()
+{
+   echo "here's the options:"
+   echo "--create-cluster create the infra and install the k8s."
+   echo "--delete-cluster     delete the k8s cluster with infra."
+   echo "--create-msrv3 create the infra,k8s cluster along with MSRv3."
+}
+
+Help
+echo "Thanks for searching the options !!"
